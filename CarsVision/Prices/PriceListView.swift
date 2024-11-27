9e20ed3bdec0a6abd7369a96643b0fb9d1ea66b2
Mekhak <mekhakghapantsyan@simplytechnologies.com>
2024-11-27 20:07:46 +0400

    pricelist update.

--- CarsVision/Prices/PriceListView.swift
+++ CarsVision/Prices/PriceListView.swift
@@ -11,35 +11,42 @@ struct PriceListView: View {
   
   @Environment(\.appModel) private var appModel
   @State private var selectedModel: NissanModel? = nil
-
+  
   let models = NissanModel.allCases
-  let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]  
+  let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
   
   var body: some View {
     ZStack {
-      ScrollView {
-        LazyVGrid(columns: columns, spacing: 16) {
-          ForEach(models, id: \.self) { model in
-            Button {
-              withAnimation(.spring()) {
-                if selectedModel == model {
-                  selectedModel = nil
-                } else {
-                  selectedModel = model
+      ScrollViewReader { proxy in
+        ScrollView {
+          LazyVGrid(columns: columns, spacing: 16) {
+            ForEach(models, id: \.self) { model in
+              Button {
+                withAnimation(.spring()) {
+                  if selectedModel == model {
+                    selectedModel = nil
+                  } else {
+                    selectedModel = model
+                  }
                 }
+              } label: {
+                PriceCell(model: model, isSelected: selectedModel == model)
+                  .id(model)
               }
-            } label: {
-              PriceCell(model: model, isSelected: selectedModel == model)
-            }
               
+            }
+          }
+          .padding()
+        }
+        .onChange(of: appModel.carModel) { oldValue, newValue in
+          withAnimation {
+            selectedModel = newValue
+            proxy.scrollTo(newValue, anchor: .center)
           }
         }
-        .padding()
       }
     }
-    .onChange(of: appModel.carModel) { oldValue, newValue in
-      selectedModel = newValue
-    }
+
   }
   
 }
